7fc482b2293b694b84c54c84f24cf81390b3a070
NightDarkness <nightdarkness62@outlook.com>
2023-12-04 01:41:04 -0700

    test

--- src/app/categorias/page.jsx
+++ src/app/categorias/page.jsx
@@ -1,4 +1,4 @@
-
+import './globals.css'
 
 export const metadata = {
     title: 'Categorias',
@@ -8,6 +8,19 @@ export const metadata = {
 export default function Categorias(){
     return(
         <>
+            <div className="title">
+                <span>Categorias</span>
+            </div>
+            <hr className='divisor'/>
+            <div className="content">
+                <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quos necessitatibus obcaecati aliquid ipsam, amet iste repellendus voluptas error nostrum omnis maiores id, harum exercitationem vero voluptatum ad labore? Iusto?
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed, eligendi deleniti ipsam adipisci fuga placeat accusamus omnis suscipit esse? Vitae aspernatur eligendi facilis deleniti fuga incidunt in nihil ad alias?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, facere? Id, consequuntur autem. Corrupti dolores asperiores, maxime illum, officia reiciendis, totam pariatur earum adipisci aut rerum incidunt quia fugiat dignissimos?
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi iure quo incidunt pariatur eum consequuntur fuga molestias excepturi soluta velit possimus error aperiam, fugit adipisci debitis, labore voluptas! Quo, sapiente?
+                    
+                </span>
+            </div>
         </>
     )
 }
